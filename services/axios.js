3c74caa53a3089b5499bef8805bbc78083d3591c
DarkDanny25 <edanuc15@gmail.com>
2025-08-13 16:18:52 -0500

    ajustando todo el frontend

--- services/axios.js
+++ services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://backend-pos-2tfc.onrender.com/api'
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
