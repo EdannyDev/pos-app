37c8c4f760b812c18c75440c38165c4330696865
DarkDanny25 <edanuc15@gmail.com>
2025-10-15 19:49:50 -0500

    producción

--- services/axios.js
+++ services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://backend-pos-q6fa.onrender.com/api'
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
